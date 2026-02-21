369f3db5540c09e45f1a685f208f52dad6143477
Wuqviy <133377143+Wuqviy@users.noreply.github.com>
2026-02-21 19:07:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -9,24 +9,24 @@ const USER_CONFIG = {
 // 请将以下代码按照图示粘贴到对应位置
 USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx921f9b98ae386608',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '428fc4dd1e9657fab060bf3bf114a669',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  //PROVINCE: '',
+  CITY: '上海',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oSevI3DHyNdQg5g2eov4Z8oOH-Z0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '	FJAP-tY9oI4bIKPdMIUpBEF4aM4-4gw7lp0RHUfA6j0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '6-16',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
